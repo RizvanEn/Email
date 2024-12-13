0b8abad42697fde38af293ab81f6026251d94e06
RizvanEn <rizvanenego@gmail.com>
2024-12-13 18:05:28 +0530

    added

--- routes/ClientRoute.js
+++ routes/ClientRoute.js
@@ -3,8 +3,9 @@ import nodemailer from 'nodemailer'
 const ClientRoute=express.Router()
 
 // POST route to send emails
+//seven wonders 
 ClientRoute.post('/sevenwonders', async (req, res) => {
-    const { from ,message,subject } = req.body;
+    const { from ,message,subject,name,number } = req.body;
 try{
       const transporter = nodemailer.createTransport({
         host: "smtp.hostinger.com",
@@ -17,11 +18,13 @@ try{
       });
   
       const mailOptions = {
-        to: "rizvanenego@gmail.com",
+        to: "contact@sevenwondersplayschool.com",
         from: 'info@sevenwondersplayschool.com',
         subject: `${subject}`,
         text: `You have an enquiry from your website\n
         Email : ${from} \n
+        Name : ${name}\n
+        Number : ${number}\n
         Message : ${message}\n
         `
       };
@@ -32,9 +35,9 @@ try{
       res.status(500).json({ message: 'Server error.' });
     }
   });
-
+//SSINFRA
   ClientRoute.post('/ssiinfra', async (req, res) => {
-    const { from ,message,subject } = req.body;
+    const { from ,message,subject,name,number } = req.body;
 try{
       const transporter = nodemailer.createTransport({
         host: "smtp.hostinger.com",
@@ -52,6 +55,72 @@ try{
         subject: `${subject}`,
         text: `You have an enquiry from your website\n
         Email : ${from} \n
+        Name : ${name}\n
+        Number : ${number}\n
+        Message : ${message}\n
+        `
+      };
+  
+      await transporter.sendMail(mailOptions);
+      res.status(200).json({ message: 'Email sent successfully.' });
+    } catch (error) {
+      res.status(500).json({ message: 'Server error.' });
+    }
+  });
+  //USPS
+  ClientRoute.post('/usps', async (req, res) => {
+    const { from ,message,subject,name,number } = req.body;
+try{
+      const transporter = nodemailer.createTransport({
+        host: "smtp.hostinger.com",
+        port: 465,
+        secure: true, // true for port 465, false for other ports
+        auth: {
+          user:process.env.USPS_USER,
+          pass: process.env.USPS_PASS,
+        },
+      });
+
+      const mailOptions = {
+        to: "contact@usps.org.in",
+        from: 'info@usps.org.in',
+        subject: `${subject}`,
+        text: `You have an enquiry from your website\n
+        Email : ${from} \n
+        Name : ${name}\n
+        Number : ${number}\n
+        Message : ${message}\n
+        `
+      };
+  
+      await transporter.sendMail(mailOptions);
+      res.status(200).json({ message: 'Email sent successfully.' });
+    } catch (error) {
+      res.status(500).json({ message: 'Server error.' });
+    }
+  });
+//MULNIVASI
+  ClientRoute.post('/mulnivasi', async (req, res) => {
+    const { from ,message,subject,name,number } = req.body;
+try{
+      const transporter = nodemailer.createTransport({
+        host: "smtp.hostinger.com",
+        port: 465,
+        secure: true, // true for port 465, false for other ports
+        auth: {
+          user:process.env.MULNIVASI_USER,
+          pass: process.env.MULNIVASI_PASS,
+        },
+      });
+  
+      const mailOptions = {
+        to: "contact@mulnivasi.in",
+        from: 'info@mulnivasi.in',
+        subject: `${subject}`,
+        text: `You have an enquiry from your website\n
+        Email : ${from} \n
+        Name : ${name}\n
+        Number : ${number}\n
         Message : ${message}\n
         `
       };
@@ -63,4 +132,37 @@ try{
     }
   });
 
+
+  //conserve earth
+  ClientRoute.post('/conserveearth', async (req, res) => {
+    const { from ,message,subject,name,number } = req.body;
+try{
+      const transporter = nodemailer.createTransport({
+        host: "smtp.hostinger.com",
+        port: 465,
+        secure: true, // true for port 465, false for other ports
+        auth: {
+          user:process.env.CONSERVE_USER,
+          pass: process.env.CONSERVE_PASS,
+        },
+      });
+  
+      const mailOptions = {
+        to: "contact@conserveearthfoundation.org",
+        from: 'info@conserveearthfoundation.org',
+        subject: `${subject}`,
+        text: `You have an enquiry from your website\n
+        Email : ${from} \n
+        Name : ${name}\n
+        Number : ${number}\n
+        Message : ${message}\n
+        `
+      };
+  
+      await transporter.sendMail(mailOptions);
+      res.status(200).json({ message: 'Email sent successfully.' });
+    } catch (error) {
+      res.status(500).json({ message: 'Server error.' });
+    }
+  });
 export default ClientRoute;
