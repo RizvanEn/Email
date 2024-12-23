db5e77d6f70828b21c6e8a11729b73e587c9e4cd
RizvanEn <rizvanenego@gmail.com>
2024-12-23 18:19:36 +0530

    bbassociates

--- routes/ClientRoute.js
+++ routes/ClientRoute.js
@@ -272,6 +272,39 @@ try{
   });
 
 
+    //BB ASSOCIATES
+    ClientRoute.post('/bbassociates', async (req, res) => {
+      const { from ,message,subject,name,number } = req.body;
+  try{
+        const transporter = nodemailer.createTransport({
+          host: "smtp.hostinger.com",
+          port: 465,
+          secure: true, // true for port 465, false for other ports
+          auth: {
+            user:process.env.BB_USER,
+            pass: process.env.BB_PASS,
+          },
+        });
+    
+        const mailOptions = {
+          to: "contact@thebbassociates.com",
+          from: 'info@thebbassociates.com',
+          subject: `${subject}`,
+          text: `You have an enquiry from your website\n
+          Email : ${from} \n
+          Name : ${name}\n
+          Number : ${number}\n
+          Message : ${message}\n
+          `
+        };
+    
+        await transporter.sendMail(mailOptions);
+        res.status(200).json({ message: 'Email sent successfully.' });
+      } catch (error) {
+        res.status(500).json({ message: 'Server error.' });
+      }
+    });
+
 
 
 
